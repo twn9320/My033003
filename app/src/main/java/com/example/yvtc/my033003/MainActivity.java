49d97b68f335308bc9f05a848e1af405362f7bac
twn9320 <twn9320@gmail.com>
2017-03-30 14:10:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/my033003/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.yvtc.my033003;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    String data[]={"蘋果","檸檬","香蕉","橘子"};
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv=(ListView)findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter();
+        lv.setAdapter(adapter);
+    }
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return data.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText(data[position]);
+            return tv;
+        }
+    }
+}
